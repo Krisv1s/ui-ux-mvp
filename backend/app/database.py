09989a6e9e0c8f0272cbcc38e1d811f1389d6f73
morcasersha <morcasersha@gmail.com>
2025-06-05 14:12:33 +0300

    add name to orders

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,7 @@ import os
 
 from initial_data import init_test_data
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app/database.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////app/database.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
